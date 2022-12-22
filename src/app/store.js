e9b56cf4130693647eb4108bf7552e7663879e2f
RolandoPranataAR <rolandopranata@gmail.com>
2022-12-23 03:58:29 +0700

    chore: setup redux

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from '../features/cart/cartSlice';
+
+// * store function serves as a link between store / reducer to all components in react
+export const store = configureStore({
+  reducer: {
+    // add all reducer here
+    cart: cartReducer,
+  },
+});
--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { store } from './app/store';
+import { Provider } from 'react-redux'; // for connect redux store into projects
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
